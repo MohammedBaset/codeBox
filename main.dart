0d473941916e340a595776c67b6ae881c90e33ea
Mohammed Baset <basetemail@gmail.com>
2025-08-20 21:28:13 +1000

    Your commit message here

--- main.dart
+++ main.dart
@@ -1,9 +1,3 @@
 void main(){ // The main() function is the entry point of every Dart program
   print("Hello world"); // Prints "Hello world" to the console
-
-List <int> age = [2, 5, 7, 8, 1, 6];
-
-for( int i = 0; i < age.length; i++) {
-  print(age[i]);
-}
 } // Ends the main() function block
